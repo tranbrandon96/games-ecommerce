e8873a7c1957c8b191b6792816ab446213f1995a
tranbrandon96 <tran.brandon@hotmail.com>
2020-09-13 01:33:51 -0700

    broken our homepage into smaller components, finished styling our menu-item component

--- game-reviews/src/components/directory/directory.component.jsx
+++ game-reviews/src/components/directory/directory.component.jsx
@@ -10,39 +10,39 @@ class Directory extends React.Component {
 
         this.state = {
             sections: [{
-                  title: 'ACTION',
+                  title: 'action',
                   imageUrl: 'https://www.nme.com/wp-content/uploads/2020/05/star-wars-jed-fallen-order-credit-ea@2000x1270-696x442.jpg',
                   id: 1,
                   linkUrl: 'shop/actions'
                 },
                 {
-                  title: 'RPG',
+                  title: 'rpg',
                   imageUrl: 'https://cdn.images.express.co.uk/img/dynamic/143/590x/Final-Fantasy-7-Remake-1277033.webp?r=1588583410924',
                   id: 2,
                   linkUrl: 'shop/RPG'
                 },
                 {
-                  title: 'SHOOTER',
+                  title: 'shooter',
                   imageUrl: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F07%2Fcall-of-duty-black-ops-cold-war-doritos-promo-leak-logo-release-info-001.jpg?q=90&w=1400&cbr=1&fit=max',
                   id: 3,
                   linkUrl: 'shop/shooter'
                 },
                 {
-                  title: 'SPORTS',
+                  title: 'sports',
                   imageUrl: 'https://www.androidcentral.com/sites/androidcentral.com/files/styles/w830/public/article_images/2020/09/2k21-3.jpg',
                   id: 4,
                   size: 'large',
                   linkUrl: 'shop/sports'
                 },
                 {
-                  title: 'RACING',
-                  imageUrl: 'http://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/media/image/2017/04/impresiones-beta-gran-turismo-sport-ps4_1.jpg',
+                  title: 'racing',
+                  imageUrl: 'https://cdn.ndtv.com/tech/gadgets/gt_sport_ps4_.jpg?output-quality=80&output-format=webp',
                   id: 5,
                   size: 'large',
                   linkUrl: 'shop/racing'
                 },
                 {
-                  title: 'ADVENTURE',
+                  title: 'adventure',
                   imageUrl: 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2020/04/AC-Valhalla-796x417.jpg',
                   id: 6,
                   size: 'large',
